7b5753303053d18291adf02e9f4cd33128371d7f
Akie Mejia <rfmejia@protonmail.com>
2016-04-06 16:19:07 -0700

    Removed validation traits

--- src/main/scala/jp/riken/massbank/reader/scala/types/Types.scala
+++ src/main/scala/jp/riken/massbank/reader/scala/types/Types.scala
@@ -1,18 +1,5 @@
 package jp.riken.massbank.reader.scala.types
 
-sealed trait Required
-trait Mandatory extends Required
-trait Optional extends Required
-
-sealed trait UI
-trait Unique extends UI
-trait Iterative extends UI
-trait Both extends Unique with Iterative
-
-sealed trait LineType
-trait SingleLine extends LineType
-trait MultipleLine extends LineType
-
 trait Peak
 case class CompletePeakTriple(mz: Double, int: Double, rel: Double) extends Peak
 case class AbsolutePeakPair(mz: Double, int: Double) extends Peak
