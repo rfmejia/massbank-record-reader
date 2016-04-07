62d16f68573bc4204b4169660ea43727e27aff6e
Akie Mejia <rfmejia@protonmail.com>
2016-04-07 14:13:22 -0700

    Tested for peakField and numPeakField parsing

--- src/test/scala/jp/riken/massbank/reader/scala/FieldParsersTest.scala
+++ src/test/scala/jp/riken/massbank/reader/scala/FieldParsersTest.scala
@@ -2,19 +2,36 @@ package jp.riken.massbank.reader.scala
 
 import jp.riken.massbank.reader.scala.types._
 import org.scalatest._
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
 
-class FieldParsersTest extends WordSpec with Matchers with FieldParsers {
+class FieldParsersTest extends WordSpec with GeneratorDrivenPropertyChecks with Matchers with FieldParsers {
   "`peakField` field parser" should {
     "correctly parse multiple lines of peaks" in {
       val input = """PK$PEAK: m/z int. rel.int.
                     |  326.65 5.3 5
                     |  328.28 7.6 7""".stripMargin
       val result = parse(peakField("PK$PEAK"), input)
-      println(input)
-      println(result)
       result shouldBe a[Success[_]]
       result.get.peaks.size shouldBe 2
       result.get.peaks shouldBe Seq(CompletePeakTriple(326.65, 5.3, 5.0), CompletePeakTriple(328.28, 7.6, 7))
+    }
+  }
+
+  "`numPeakField` parser" should {
+    "accept integer inputs" in {
+      forAll(Generators.validInt) { i: Int =>
+        val result = parse(numPeakField("PK$NUM_PEAK"), "PK$NUM_PEAK: " + i.toString)
+        result shouldBe a[Success[_]]
+        result.get shouldBe Some(i)
+      }
+    }
+
+    "reject non-integer inputs" in {
+      forAll(Generators.validString) { s: String =>
+        val result = parseAll(numPeakField("PK$NUM_PEAK"), "PK$NUM_PEAK: " + s)
+        result shouldBe a[Success[_]]
+        result.get shouldBe None
+      }
     }
   }
 }
