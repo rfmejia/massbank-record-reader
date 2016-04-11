acc7afcfca43ed580581039d0a5b8fe5e72e0a03
Akie Mejia <rfmejia@protonmail.com>
2016-04-11 11:43:52 -0700

    Use util.Try to represent results instead of an Either

--- src/main/scala/jp/riken/massbank/reader/scala/MassBankRecordReader.scala
+++ src/main/scala/jp/riken/massbank/reader/scala/MassBankRecordReader.scala
@@ -2,16 +2,18 @@ package jp.riken.massbank.reader.scala
 
 import jp.riken.massbank.reader.scala.parsers.MassBankRecordParser
 import jp.riken.massbank.reader.scala.types.MassBankRecord
-
 import scala.io.Source
+import scala.util.Try
+
+class MassBankRecordParsingException(val message: String) extends IllegalArgumentException(message)
 
 object MassBankRecordReader extends MassBankRecordParser {
-  def read(input: String): Either[String, MassBankRecord] = parseAll(massBankRecord, input) match {
-    case Success(record, _)      => Right(record)
-    case NoSuccess(error, input) => Left(error + " " + input.toString)
+  def read(input: String): Try[MassBankRecord] = parseAll(massBankRecord, input) match {
+    case Success(record, _)      => util.Success(record)
+    case NoSuccess(error, input) => util.Failure(new MassBankRecordParsingException(error + " " + input.toString))
   }
 
-  def read(src: Source): Either[String, MassBankRecord] = {
+  def read(src: Source): Try[MassBankRecord] = {
     val lines = src.getLines.mkString("\n")
     read(lines)
   }
--- src/test/scala/jp/riken/massbank/reader/scala/parsers/MassBankRecordParserTest.scala
+++ src/test/scala/jp/riken/massbank/reader/scala/parsers/MassBankRecordParserTest.scala
@@ -5,22 +5,23 @@ import jp.riken.massbank.reader.scala.types.MassBankRecord
 import org.scalatest._
 
 import scala.io.Source
+import scala.util.Try
 
 class MassBankRecordParserTest extends WordSpec with Matchers with MassBankRecordParser {
-  "An `MassBankRecordGroupParser`" should {
+  "A `MassBankRecordParser`" should {
 
     "correctly parse sample data" in {
       val sources = Source.fromURL(getClass.getResource("/testdata"))
       sources should not be null
 
-      def parseFiles(filename: String): Either[String, MassBankRecord] = {
+      def parseFiles(filename: String): Try[MassBankRecord] = {
         val source = Source.fromURL(getClass.getResource("/testdata/" + filename))
         source should not be null
         MassBankRecordReader.read(source)
       }
 
       val filenames = sources.getLines
-      filenames.map(parseFiles).foreach(_ shouldBe a[Right[_, _]])
+      filenames.map(parseFiles).foreach(_ shouldBe a[util.Success[_]])
     }
 
   }
